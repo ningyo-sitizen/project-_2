6db352516e8b23164c63873545a869b2907a9eac
ningyo-sitizen <jowymone26@gmail.com>
2025-03-19 03:24:09 +0700

    menu page sudah ditambah

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function showCategory(category) {
+    document.getElementById('foods').style.display = 'none';
+    document.getElementById('drinks').style.display = 'none';
+    document.getElementById('dessert').style.display = 'none';
+    document.getElementById(category).style.display = 'grid';
+
+    document.querySelectorAll('.nav a').forEach(a => a.classList.remove('active'));
+    event.target.classList.add('active');
+}
